eb40f108e9e816a362ee7062a14010b0c3c70367
pkirilin <kirilin.pav@gmail.com>
2018-12-31 20:28:24 +0300

    Веб-приложение. Файловая настройка

--- Strive/Strive.Web.App/Startup.cs
+++ Strive/Strive.Web.App/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace Strive.Web.App
 {
@@ -19,6 +21,26 @@ namespace Strive.Web.App
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            #region Files
+
+            // Подключение файлов по умолчанию
+            app.UseDefaultFiles();
+
+            // Подключение статических файлов (для <link> и <script>)
+            app.UseStaticFiles();
+
+            // Поддержка каталога node_modules для npm
+            //app.UseFileServer(new FileServerOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(
+            //        Path.Combine(env.ContentRootPath, "node_modules")
+            //    ),
+            //    RequestPath = "/node_modules",
+            //    EnableDirectoryBrowsing = false
+            //});
+
+            #endregion
+
             app.UseMvc(ConfigureRoutesAction);
         }
 
