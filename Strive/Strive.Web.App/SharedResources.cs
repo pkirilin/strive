176e16b26ea56a6977e7816f3d689b0ec751ea62
pkirilin <kirilin.pav@gmail.com>
2019-01-06 21:23:45 +0300

    Веб-приложение. Добавлены общие ресурсы локализации
    Веб-приложение. Добавлены общие ресурсы en-локализации

---
+++ Strive/Strive.Web.App/SharedResources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Strive.Web.App
+{
+    /// <summary>
+    /// Общие ресурсы локализации
+    /// </summary>
+    public class SharedResources
+    {
+    }
+}
