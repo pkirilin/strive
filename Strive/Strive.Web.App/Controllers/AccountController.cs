a3e033236a5b409ae36f03daf099b72ba6b22c33
pkirilin <kirilin.pav@gmail.com>
2019-01-12 21:05:34 +0300

    AccountController. Созданы get и post версии для методов Login и Register

--- Strive/Strive.Web.App/Controllers/AccountController.cs
+++ Strive/Strive.Web.App/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
+using Strive.Data.ViewModels.Account.Login;
 
 namespace Strive.Web.App.Controllers
 {
@@ -11,20 +12,43 @@ namespace Strive.Web.App.Controllers
     {
         private readonly IStringLocalizer<AccountController> _localizer;
 
+        #region PrivateMethods
+
+        private void InitLoginViewData()
+        {
+            ViewData["TitleSecondary"] = _localizer["TitleSecondaryLogin"];
+        }
+
+        private void InitRegisterViewData()
+        {
+            ViewData["TitleSecondary"] = _localizer["TitleSecondaryRegister"];
+        }
+
+        #endregion
+
         public AccountController(IStringLocalizer<AccountController> plocalizer)
         {
             _localizer = plocalizer;
         }
 
+        [HttpGet]
         public IActionResult Login()
         {
-            ViewData["TitleSecondary"] = _localizer["TitleSecondaryLogin"];
+            InitLoginViewData();
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(LoginViewModel pmodel)
+        {
+            InitLoginViewData();
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
-            ViewData["TitleSecondary"] = _localizer["TitleSecondaryRegister"];
+            InitRegisterViewData();
             return View();
         }
     }
