b9c3fbd776675af77712a80049d82c4d6cb2a690
pkirilin <kirilin.pav@gmail.com>
2019-01-12 21:01:44 +0300

    ViewModels. Добавлены модели представлений для Login и Register

---
+++ Strive/Strive.Data/ViewModels/Account/LoginRegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strive.Data.ViewModels.Account
+{
+    public class LoginRegisterViewModel
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Strive/Strive.Data/ViewModels/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strive.Data.ViewModels.Account.Register
+{
+    public class RegisterViewModel : LoginRegisterViewModel
+    {
+        public string PasswordConfirm { get; set; }
+    }
+}
