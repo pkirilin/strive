54f0a28d7bdeabd8b90e70d525964f08ff6fd323
pkirilin <kirilin.pav@gmail.com>
2019-01-12 16:31:26 +0300

    Entities. Добавлена сущность UserDetails

---
+++ Strive/Strive.Data/Entities/UserDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strive.Data.Entities
+{
+    /// <summary>
+    /// Сущность "Данные пользователя". Описывает дополнительные данные о пользователе системы
+    /// </summary>
+    public class UserDetails
+    {
+        #region DataProperties
+
+        /// <summary>
+        /// ID данных пользователя
+        /// </summary>
+        public int UserDetailsID { get; set; }
+
+        #endregion
+
+        #region ReferenceProperties
+
+        /// <summary>
+        /// ID пользователя, которому принадлежат данные
+        /// </summary>
+        public int UserID { get; set; }
+
+        #endregion
+
+        #region NavigationProperties
+
+        /// <summary>
+        /// Ссылка на пользователя, которому принадлежат данные
+        /// </summary>
+        public User User { get; set; }
+
+        #endregion
+    }
+}
