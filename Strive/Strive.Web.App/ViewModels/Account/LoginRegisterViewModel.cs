40f7c6bacdb680f97d6376e64b77920aa63790f6
pkirilin <kirilin.pav@gmail.com>
2019-01-13 17:38:49 +0300

    ViewModels. Login, Register. Добавлены основные атрибуты валидации

--- Strive/Strive.Web.App/ViewModels/Account/LoginRegisterViewModel.cs
+++ Strive/Strive.Web.App/ViewModels/Account/LoginRegisterViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Strive.Web.App.ViewModels.Account
 {
     public class LoginRegisterViewModel
     {
+        [Required]
+        [RegularExpression(@"^[A-Za-z][A-Za-z0-9]{2,63}@[a-z][a-z0-9]{2,246}\.[a-z]{2,4}")]
         public string Email { get; set; }
 
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
--- Strive/Strive.Web.App/ViewModels/Account/Register/RegisterViewModel.cs
+++ Strive/Strive.Web.App/ViewModels/Account/Register/RegisterViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Strive.Web.App.ViewModels.Account.Register
 {
     public class RegisterViewModel : LoginRegisterViewModel
     {
+        [Required]
+        // @todo [Remote]
         public string Username { get; set; }
 
+        [Required]
+        [Compare("Password")]
         public string PasswordConfirm { get; set; }
     }
 }
