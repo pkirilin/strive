f96312af8f3a4df45064c588c1de24b01630a63d
pkirilin <kirilin.pav@gmail.com>
2019-01-16 23:34:45 +0300

    Account ViewModels. Добавлены комментарии для существующих моделей представления

--- Strive/Strive.Web.App/ViewModels/Account/Login/LoginViewModel.cs
+++ Strive/Strive.Web.App/ViewModels/Account/Login/LoginViewModel.cs
@@ -4,10 +4,19 @@ using System.Text;
 
 namespace Strive.Web.App.ViewModels.Account.Login
 {
+    /// <summary>
+    /// Модель представления для входа пользователя в систему
+    /// </summary>
     public class LoginViewModel : LoginRegisterViewModel
     {
+        /// <summary>
+        /// Параметр, показывающий, нужно ли сохранять куки пользователя после закрытия браузера
+        /// </summary>
         public bool RememberMe { get; set; }
 
+        /// <summary>
+        /// Адрес возврата пользователя на запрашиваемый URL после успешной авторизации
+        /// </summary>
         public string ReturnUrl { get; set; }
     }
 }
--- Strive/Strive.Web.App/ViewModels/Account/Register/RegisterViewModel.cs
+++ Strive/Strive.Web.App/ViewModels/Account/Register/RegisterViewModel.cs
@@ -5,12 +5,21 @@ using System.Text;
 
 namespace Strive.Web.App.ViewModels.Account.Register
 {
+    /// <summary>
+    /// Модель представления для регистрации пользователя в системе
+    /// </summary>
     public class RegisterViewModel : LoginRegisterViewModel
     {
+        /// <summary>
+        /// Имя пользователя/Логин
+        /// </summary>
         [Required]
         // @todo [Remote]
         public string Username { get; set; }
 
+        /// <summary>
+        /// Строка для повторного ввода пароля и его подтверждения
+        /// </summary>
         [Required]
         [Compare("Password")]
         public string PasswordConfirm { get; set; }
