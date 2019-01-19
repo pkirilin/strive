88011ab423f33f1666a1c7f8d12e096b60bad0e2
pkirilin <kirilin.pav@gmail.com>
2019-01-19 22:44:50 +0300

    Добавлен функционал по отправке email сообщений для подтверждения регистрации

---
+++ Strive/Strive.Communication.Emails/EmailBuilders/ConfirmRegistrationEmailBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strive.Communication.Emails.EmailBuilders
+{
+    /// <summary>
+    /// Реализует функционал, необходимый для построения email сообщений
+    /// подтверждения регистрации учетных записей пользователей
+    /// </summary>
+    public class ConfirmRegistrationEmailBuilder : EmailBuilder
+    {
+        private readonly string _emailConfirmationLink;
+
+        public ConfirmRegistrationEmailBuilder(string pemailConfirmationLink) : base()
+        {
+            _emailConfirmationLink = pemailConfirmationLink;
+        }
+
+        public override void SetSubject()
+        {
+            // @todo locale
+            _message.Subject = "Strive register";
+        }
+
+        public override void SetBody()
+        {
+            // @todo locale
+            AddBodyTextPart($"Strive register <a href=\"{_emailConfirmationLink}\">link</a>");
+            base.SetBody();
+        }
+    }
+}
