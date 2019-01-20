d88a6ab123776c6d3be02f252b524a7be0183c34
pkirilin <kirilin.pav@gmail.com>
2019-01-20 22:38:45 +0300

    ViewModels. Добавлена модель представления для запроса на сброс пароля

---
+++ Strive/Strive.Web.App/ViewModels/Account/ForgotPassword/ForgotPasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Strive.Web.App.ViewModels.Account.ForgotPassword
+{
+    /// <summary>
+    /// Модель представления для страницы запроса сброса пароля
+    /// </summary>
+    public class ForgotPasswordViewModel
+    {
+        /// <summary>
+        /// Адрес электронной почты пользователя
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
