69bcc60a74ee0262bbe3991f3b9fb671c70a5bf2
pkirilin <kirilin.pav@gmail.com>
2019-01-30 23:35:13 +0300

    Startup. Добавлена настройка аутентификации для Identity, исправлен путь redirect'а на страницу логина для неавторизованных пользователей

--- Strive/Strive.Web.App/Startup.cs
+++ Strive/Strive.Web.App/Startup.cs
@@ -11,63 +11,66 @@ using Strive.Web.Common.Providers;
 
 namespace Strive.Web.App
 {
-    public class Startup
-    {
-        public Startup(IConfiguration pconfig)
-        {
-            this.Configuration = pconfig;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration pconfig)
+		{
+			this.Configuration = pconfig;
+		}
 
-        public IConfiguration Configuration { get; private set; }
+		public IConfiguration Configuration { get; private set; }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Добавление локализации приложения
-            services.AddLocalization(StartupActionProvider.SetupLocalizationAction);
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Добавление локализации приложения
+			services.AddLocalization(StartupActionProvider.SetupLocalizationAction);
 
-            // Добавление параметров локализации
-            services.Configure<RequestLocalizationOptions>(StartupActionProvider.ConfigureRequestLocalizationOptionsAction);
+			// Добавление параметров локализации
+			services.Configure<RequestLocalizationOptions>(StartupActionProvider.ConfigureRequestLocalizationOptionsAction);
 
-            // Добавление аутентификации на основе кук
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(StartupActionProvider.ConfigureCookieAuthenticationOptionsAction);
+			// Добавление аутентификации на основе кук
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+				.AddCookie(StartupActionProvider.ConfigureCookieAuthenticationOptionsAction);
 
-            // Добавление дополнительных настроек маршрутизации
-            services.AddRouting(StartupActionProvider.ConfigureRoutingOptionsAction);
+			// Добавление дополнительных настроек маршрутизации
+			services.AddRouting(StartupActionProvider.ConfigureRoutingOptionsAction);
 
-            // Добавление контекста данных для работы с БД
-            services.AddDbContext<StriveDbContext>(options =>
-                options.UseSqlServer(
-                    this.Configuration.GetConnectionString("DefaultConnection")));
+			// Добавление контекста данных для работы с БД
+			services.AddDbContext<StriveDbContext>(options =>
+				options.UseSqlServer(
+					this.Configuration.GetConnectionString("DefaultConnection")));
 
-            // Добавление Identity
-            services.AddIdentity<User, IdentityRole>(StartupActionProvider.SetupIdentityOptionsAction)
-                .AddEntityFrameworkStores<StriveDbContext>()
-                .AddDefaultTokenProviders();
+			// Добавление Identity
+			services.AddIdentity<User, IdentityRole>(StartupActionProvider.SetupIdentityOptionsAction)
+				.AddEntityFrameworkStores<StriveDbContext>()
+				.AddDefaultTokenProviders();
 
-            // Добавление MVC
-            services.AddMvc()
-                .AddViewLocalization()
-                .AddDataAnnotationsLocalization();
-        }
+			// Настройки параметров аутентификации на основе кук для Identity
+			services.ConfigureApplicationCookie(StartupActionProvider.ConfigureCookieAuthenticationOptionsAction);
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            // Подключение файлов по умолчанию
-            app.UseDefaultFiles();
+			// Добавление MVC
+			services.AddMvc()
+				.AddViewLocalization()
+				.AddDataAnnotationsLocalization();
+		}
 
-            // Подключение статических файлов (для <link> и <script>)
-            app.UseStaticFiles();
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			// Подключение файлов по умолчанию
+			app.UseDefaultFiles();
 
-            // Настройка параметров локализации, которые потом могут быть использованы в приложении
-            app.UseRequestLocalization(app.ApplicationServices
-                .GetService<IOptions<RequestLocalizationOptions>>().Value);
+			// Подключение статических файлов (для <link> и <script>)
+			app.UseStaticFiles();
 
-            // Подключение аутентификации
-            app.UseAuthentication();
+			// Настройка параметров локализации, которые потом могут быть использованы в приложении
+			app.UseRequestLocalization(app.ApplicationServices
+				.GetService<IOptions<RequestLocalizationOptions>>().Value);
 
-            // Подключение MVC
-            app.UseMvc(StartupActionProvider.ConfigureRoutesAction);
-        }
-    }
+			// Подключение аутентификации
+			app.UseAuthentication();
+
+			// Подключение MVC
+			app.UseMvc(StartupActionProvider.ConfigureRoutesAction);
+		}
+	}
 }
--- Strive/Strive.Web.Common/Providers/StartupActionProvider.cs
+++ Strive/Strive.Web.Common/Providers/StartupActionProvider.cs
@@ -2,90 +2,86 @@
 using System.Globalization;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Localization;
-using Strive.Web.Common.Utils;
 
 namespace Strive.Web.Common.Providers
 {
-    /// <summary>
-    /// Методы действий для настройки приложения, использующиеся в классе Startup
-    /// </summary>
-    public static class StartupActionProvider
-    {
-        /// <summary>
-        /// Действие настройки маршрутизации приложения
-        /// </summary>
-        public static void ConfigureRoutesAction(IRouteBuilder prouteBuilder)
-        {
-            prouteBuilder.MapRoute("default", "{controller=home}/{action=index}/{id?}");
-        }
+	/// <summary>
+	/// Методы действий для настройки приложения, использующиеся в классе Startup
+	/// </summary>
+	public static class StartupActionProvider
+	{
+		/// <summary>
+		/// Действие настройки маршрутизации приложения
+		/// </summary>
+		public static void ConfigureRoutesAction(IRouteBuilder prouteBuilder)
+		{
+			prouteBuilder.MapRoute("default", "{controller=home}/{action=index}/{id?}");
+		}
 
-        /// <summary>
-        /// Действие дополнительных настроек маршрутизации
-        /// </summary>
-        public static void ConfigureRoutingOptionsAction(RouteOptions prouteOptions)
-        {
-            // Подключение средства преобразования camel-case путей в slug-пути
-            prouteOptions.ConstraintMap["slugify"] = typeof(SlugifyParameterTransformer);
+		/// <summary>
+		/// Действие дополнительных настроек маршрутизации
+		/// </summary>
+		public static void ConfigureRoutingOptionsAction(RouteOptions prouteOptions)
+		{
+			// Понижение регистра всех путей
+			prouteOptions.LowercaseUrls = true;
+		}
 
-            // Понижение регистра всех путей
-            prouteOptions.LowercaseUrls = true;
-        }
+		/// <summary>
+		/// Действие настройки локализации приложения
+		/// </summary>
+		public static void SetupLocalizationAction(LocalizationOptions poptions)
+		{
+			poptions.ResourcesPath = "Resources";
+		}
 
-        /// <summary>
-        /// Действие настройки локализации приложения
-        /// </summary>
-        public static void SetupLocalizationAction(LocalizationOptions poptions)
-        {
-            poptions.ResourcesPath = "Resources";
-        }
+		/// <summary>
+		/// Действие настройки параметров локализации, которые можно будет в дальнейшем получать в приложении
+		/// </summary>
+		public static void ConfigureRequestLocalizationOptionsAction(RequestLocalizationOptions poptions)
+		{
+			// Культуры, поддерживаемые в приложении
+			var supportedCultures = new List<CultureInfo>()
+			{
+				new CultureInfo("en"),
+				new CultureInfo("ru")
+			};
 
-        /// <summary>
-        /// Действие настройки параметров локализации, которые можно будет в дальнейшем получать в приложении
-        /// </summary>
-        public static void ConfigureRequestLocalizationOptionsAction(RequestLocalizationOptions poptions)
-        {
-            // Культуры, поддерживаемые в приложении
-            var supportedCultures = new List<CultureInfo>()
-            {
-                new CultureInfo("en"),
-                new CultureInfo("ru")
-            };
+			poptions.DefaultRequestCulture = new RequestCulture("en");
+			poptions.SupportedCultures = supportedCultures;
+			poptions.SupportedUICultures = supportedCultures;
+		}
 
-            poptions.DefaultRequestCulture = new RequestCulture("en");
-            poptions.SupportedCultures = supportedCultures;
-            poptions.SupportedUICultures = supportedCultures;
-        }
+		/// <summary>
+		/// Действие настройки параметров аутентификации на основе кук
+		/// </summary>
+		public static void ConfigureCookieAuthenticationOptionsAction(CookieAuthenticationOptions pcookieAuthenticationOptions)
+		{
+			pcookieAuthenticationOptions.LoginPath = "/account/login";
+			pcookieAuthenticationOptions.ReturnUrlParameter = "returnUrl";
+		}
 
-        /// <summary>
-        /// Действие настройки параметров аутентификации на основе кук
-        /// </summary>
-        public static void ConfigureCookieAuthenticationOptionsAction(CookieAuthenticationOptions pcookieAuthenticationOptions)
-        {
-            pcookieAuthenticationOptions.LoginPath = new PathString("/account/login");
-        }
+		/// <summary>
+		/// Действие настройки параметров Identity
+		/// </summary>
+		public static void SetupIdentityOptionsAction(IdentityOptions pidentityOptions)
+		{
+			// @todo реализовать свои IPasswordValidator, IUserValidator
 
-        /// <summary>
-        /// Действие настройки параметров Identity
-        /// </summary>
-        public static void SetupIdentityOptionsAction(IdentityOptions pidentityOptions)
-        {
-            // @todo реализовать свои IPasswordValidator, IUserValidator
+			// Настройки валидации пользователя
+			pidentityOptions.User.RequireUniqueEmail = true;
 
-            // Настройки валидации пользователя
-            pidentityOptions.User.RequireUniqueEmail = true;
+			// Настройки валидации пароля
+			pidentityOptions.Password.RequiredLength = 1;
+			pidentityOptions.Password.RequireNonAlphanumeric = false;
+			pidentityOptions.Password.RequireDigit = false;
+			pidentityOptions.Password.RequireUppercase = false;
+			pidentityOptions.Password.RequireLowercase = false;
 
-            // Настройки валидации пароля
-            pidentityOptions.Password.RequiredLength = 1;
-            pidentityOptions.Password.RequireNonAlphanumeric = false;
-            pidentityOptions.Password.RequireDigit = false;
-            pidentityOptions.Password.RequireUppercase = false;
-            pidentityOptions.Password.RequireLowercase = false;
-            
-        }
-    }
+		}
+	}
 }
