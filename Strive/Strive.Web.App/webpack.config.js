3a1df8228fba2d81c9c0ad18571fa547afe3b27c
pkirilin <kirilin.pav@gmail.com>
2019-02-02 18:21:23 +0300

    Веб-приложение. Создание конфигов для npm и webpack

---
+++ Strive/Strive.Web.App/webpack.config.js
@@ -0,0 +1,30 @@
+"use strict";
+{
+  const path = require("path");
+  const bundleFolder = "wwwroot/build/";
+  const CleanWebpackPlugin = require("clean-webpack-plugin");
+  const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+  module.exports = {
+    mode: "development",
+    entry: "./Scripts/index.js",
+    output: {
+      filename: "app.js",
+      path: path.resolve(__dirname, bundleFolder),
+      publicPath: bundleFolder
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader"
+        },
+        {
+          test: /\.css$/,
+          use: ["vue-style-loader", "css-loader"]
+        }
+      ]
+    },
+    plugins: [new CleanWebpackPlugin([bundleFolder]), new VueLoaderPlugin()]
+  };
+}
