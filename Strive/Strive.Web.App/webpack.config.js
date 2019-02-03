6e75466dab1a26d062c45c6c72146ec7c6b95222
pkirilin <kirilin.pav@gmail.com>
2019-02-03 17:39:08 +0300

    webpack. Добавление jQuery, popper.js, bootstrap. Сборка js-файлов

--- Strive/Strive.Web.App/webpack.config.js
+++ Strive/Strive.Web.App/webpack.config.js
@@ -5,26 +5,51 @@
   const CleanWebpackPlugin = require("clean-webpack-plugin");
   const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
-  module.exports = {
-    mode: "development",
-    entry: { main: "./Scripts/index.js" },
-    output: {
-      filename: "app.js",
-      path: path.resolve(__dirname, bundleFolder),
-      publicPath: bundleFolder
+  module.exports = [
+    // app
+    {
+      entry: { main: "./Scripts/index.js" },
+      mode: "development",
+      output: {
+        filename: "app.js",
+        path: path.resolve(__dirname, bundleFolder),
+        publicPath: bundleFolder
+      },
+      module: {
+        rules: [
+          {
+            test: /\.vue$/,
+            loader: "vue-loader"
+          },
+          {
+            test: /\.css$/,
+            use: ["vue-style-loader", "css-loader"]
+          }
+        ]
+      },
+      plugins: [new CleanWebpackPlugin([bundleFolder]), new VueLoaderPlugin()]
     },
-    module: {
-      rules: [
-        {
-          test: /\.vue$/,
-          loader: "vue-loader"
-        },
-        {
-          test: /\.css$/,
-          use: ["vue-style-loader", "css-loader"]
-        }
-      ]
+    // jQuery
+    {
+      entry: { jquery: "./node_modules/jquery" },
+      mode: "development",
+      output: {
+        filename: "jquery.js",
+        path: path.resolve(__dirname, bundleFolder),
+        publicPath: bundleFolder
+      },
+      plugins: [new CleanWebpackPlugin(bundleFolder)]
     },
-    plugins: [new CleanWebpackPlugin([bundleFolder]), new VueLoaderPlugin()]
-  };
+    // bootstrap
+    {
+      entry: { bootstrap: "./node_modules/bootstrap" },
+      mode: "development",
+      output: {
+        filename: "boostrap.js",
+        path: path.resolve(__dirname, bundleFolder),
+        publicPath: bundleFolder
+      },
+      plugins: [new CleanWebpackPlugin(bundleFolder)]
+    }
+  ];
 }
