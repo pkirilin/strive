07734ffd2fc9431f9dd3eb5c46b9ff867712ae2f
pkirilin <kirilin.pav@gmail.com>
2019-02-16 19:19:03 +0300

    Components. Добавлен компонент для вывода списка ошибок валидации

---
+++ Strive/Strive.Web.App/Components/Shared/ValidationErrorsSummary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Strive.Web.App.Components.Shared
+{
+	public class ValidationErrorsSummary : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
