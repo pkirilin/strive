75abc09871c7bc12ffd5ead6aa0e7fbb76ff17cf
pkirilin <kirilin.pav@gmail.com>
2019-03-02 16:02:51 +0300

    Preparing layouts for Home and Account

--- react/strive-client/src/App/App.jsx
+++ react/strive-client/src/App/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Router, Switch, Route } from "react-router";
+import { history } from "../_helpers";
+import { Home } from "../Home";
+import { Account } from "../Account";
 
 export class App extends React.Component {
   render() {
-    return <div>Strive</div>;
+    return (
+      <div>
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/account" component={Account} />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
