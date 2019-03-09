37504eb07eff8dce2e477f40285e1088f0d6cd2c
pkirilin <kirilin.pav@gmail.com>
2019-03-09 13:43:44 +0300

    Added redux infrastructure

--- react/strive-client/src/index.js
+++ react/strive-client/src/index.js
@@ -1,12 +1,19 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./_helpers";
 import { App } from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
