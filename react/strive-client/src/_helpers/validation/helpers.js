9898541e16ce14ef047012b94f244076194a7068
pkirilin <kirilin.pav@gmail.com>
2019-03-18 13:21:21 +0300

    focusFirstInvalidField. Added return value

--- react/strive-client/src/_helpers/validation/helpers.js
+++ react/strive-client/src/_helpers/validation/helpers.js
@@ -4,10 +4,17 @@ export const validationHelpers = {
   focusFirstInvalidField
 };
 
-/** Focuses first invalid input field inside form with target selector */
+/** Focuses first invalid input field inside form with target selector
+ *
+ * @returns {boolean}
+ * true: at least one invalid field was found,
+ * false: no invalid fields were found
+ */
 function focusFirstInvalidField(formSelector) {
   let invalidElem = document.querySelector(`${formSelector} .is-invalid`);
   if (invalidElem !== null) {
     invalidElem.focus();
+    return true;
   }
+  return false;
 }
