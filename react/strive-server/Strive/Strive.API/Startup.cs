577a802370e70741ea45532b9699b37eb6e5a7a7
pkirilin <kirilin.pav@gmail.com>
2019-03-25 22:40:50 +0300

    Added CORS, default policy for client. Configured server for mvc/web api

--- react/strive-server/Strive/Strive.API/Startup.cs
+++ react/strive-server/Strive/Strive.API/Startup.cs
@@ -1,23 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Strive.API
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services)
+		public Startup(IConfiguration configuration)
 		{
+			this.Configuration = configuration;
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddCors(options => options.AddPolicy("AllowClientApp", builder => 
+				builder.WithOrigins(this.Configuration.GetSection("ClientAppConfig")["Host"])
+					.AllowAnyHeader()
+					.AllowAnyMethod()
+					.AllowCredentials()));
+
+			services.AddMvc();
+		}
+
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -25,10 +32,8 @@ namespace Strive.API
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.Run(async (context) =>
-			{
-				await context.Response.WriteAsync("Hello World!");
-			});
+			app.UseCors("AllowClientApp");
+			app.UseMvc();
 		}
 	}
 }
