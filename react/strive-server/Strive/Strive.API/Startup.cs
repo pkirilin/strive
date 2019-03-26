16c8ef62c2089ef9435da422e59729255840415b
pkirilin <kirilin.pav@gmail.com>
2019-03-26 16:26:29 +0300

    Configured Startup JWT auth

--- react/strive-server/Strive/Strive.API/Startup.cs
+++ react/strive-server/Strive/Strive.API/Startup.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Strive.API
 {
@@ -16,11 +19,30 @@ namespace Strive.API
 
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddCors(options => options.AddPolicy("AllowClientApp", builder => 
+			services.AddCors(options => options.AddPolicy("AllowClientApp", builder =>
 				builder.WithOrigins(this.Configuration.GetSection("clientAppConfig")["host"])
 					.AllowAnyHeader()
 					.AllowAnyMethod()
 					.AllowCredentials()));
+
+			// Configure JWT authentication
+			services.AddAuthentication(options =>
+				{
+					options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+					options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+				})
+				.AddJwtBearer(options =>
+				{
+					options.RequireHttpsMetadata = false;
+					options.SaveToken = true;
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("secret")),
+						ValidateIssuer = false,
+						ValidateAudience = false
+					};
+				});
 
 			services.AddMvc();
 		}
@@ -33,6 +55,9 @@ namespace Strive.API
 			}
 
 			app.UseCors("AllowClientApp");
+
+			app.UseAuthentication();
+
 			app.UseMvc();
 		}
 	}
