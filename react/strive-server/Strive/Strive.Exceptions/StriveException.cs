dbccf1fbd4446d2ec057c9c448442fd5edee5169
pkirilin <kirilin.pav@gmail.com>
2019-03-29 17:12:59 +0300

    Added general app exception class

---
+++ react/strive-server/Strive/Strive.Exceptions/StriveException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Strive.Exceptions
+{
+	/// <summary>
+	/// Base app exception class
+	/// </summary>
+	public class StriveException : Exception
+	{
+		public StriveException(string message) : base(message)
+		{
+		}
+	}
+}
