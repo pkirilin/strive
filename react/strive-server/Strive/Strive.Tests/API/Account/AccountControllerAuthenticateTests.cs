cc357ccbbdb057685b659ff3f329250cb2872609
Pavel Kirilin <kirilin.pav@gmail.com>
2019-04-01 15:13:42 +0300

    Added unit tests for AccountController Authenticate method

---
+++ react/strive-server/Strive/Strive.Tests/API/Account/AccountControllerAuthenticateTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Strive.Data.Dtos;
+using Strive.Data.Entities;
+using Xunit;
+
+namespace Strive.Tests.API.Account
+{
+	public class AccountControllerAuthenticateTests : AccountControllerTests
+	{
+		[Fact]
+		public void UserUnauthorized()
+		{
+			var loginRequest = new UserLoginRequestDto()
+			{
+				Username = "username",
+				Password = "password"
+			};
+
+			_accountServiceMock
+				.Setup(service => service.Authenticate(It.IsNotNull<string>(), It.IsNotNull<string>()))
+				.Returns(() => null);
+
+			IActionResult result = this.AccountControllerInstance.Authenticate(loginRequest);
+
+			Assert.IsType<UnauthorizedResult>(result);
+		}
+
+		[Fact]
+		public void TokenCreatedWithoutException()
+		{
+			var loginRequest = new UserLoginRequestDto()
+			{
+				Username = "username",
+				Password = "password"
+			};
+
+			_accountServiceMock
+				.Setup(service => service.Authenticate(loginRequest.Username, loginRequest.Password))
+				.Returns(() => new User());
+
+			IActionResult result = this.AccountControllerInstance.Authenticate(loginRequest);
+
+			Assert.IsType<OkObjectResult>(result);
+		}
+	}
+}
