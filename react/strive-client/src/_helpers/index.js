7a1dc5fb36d7c9b2b9b70c3336308d72a5bfcbac
Pavel Kirilin <kirilin.pav@gmail.com>
2019-04-06 22:35:43 +0300

    [client] Added config helper

--- react/strive-client/src/_helpers/index.js
+++ react/strive-client/src/_helpers/index.js
@@ -2,3 +2,4 @@ export { history } from "./history";
 export { store } from "./store";
 export { commonHelpers } from "./common";
 export { getResourcesForCurrentCulture } from "./resources";
+export { config } from "./config";
