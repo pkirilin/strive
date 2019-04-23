9879c3acf84bb7e53ea765ad68518767fe345ce7
Pavel Kirilin <kirilin.pav@gmail.com>
2019-04-23 12:33:00 +0300

    [client] Replaced custom spinners with bootstrap spinners

--- react/strive-client/src/_components/Loading.jsx
+++ react/strive-client/src/_components/Loading.jsx
@@ -7,44 +7,9 @@ export class Loading extends React.Component {
 
   render() {
     return (
-      <div className="d-flex justify-content-center">
-        <svg
-          width="50px"
-          height="50px"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 100 100"
-          preserveAspectRatio="xMidYMid"
-          className="lds-rolling"
-        >
-          <circle
-            cx="50"
-            cy="50"
-            fill="none"
-            ng-attr-stroke="{{config.color}}"
-            ng-attr-stroke-width="{{config.width}}"
-            ng-attr-r="{{config.radius}}"
-            ng-attr-stroke-dasharray="{{config.dasharray}}"
-            stroke="#dce4eb"
-            strokeWidth="10"
-            r="35"
-            strokeDasharray="164.93361431346415 56.97787143782138"
-            transform="rotate(323.814 50 50)"
-          >
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              calcMode="linear"
-              values="0 50 50;360 50 50"
-              keyTimes="0;1"
-              dur="1s"
-              begin="0s"
-              repeatCount="indefinite"
-            />
-          </circle>
-        </svg>
-        <div className="d-flex align-items-center p-2 bg-transparent text-muted">
-          {this.props.text}
-        </div>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="spinner-border text-muted" />
+        <span className="p-2 bg-transparent text-muted">{this.props.text}</span>
       </div>
     );
   }
