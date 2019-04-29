4a17bb674f050a44acaf0912f6deaebeabcce0e5
Pavel Kirilin <kirilin.pav@gmail.com>
2019-04-29 21:44:50 +0300

    [server] Created empty ProjectsController and its tests

---
+++ react/strive-server/Strive/Strive.API/Controllers/ProjectsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Strive.API.Controllers
+{
+	[Authorize]
+	[Route("[controller]")]
+	[ApiController]
+	public class ProjectsController : ControllerBase
+	{
+		public ProjectsController()
+		{
+		}
+
+		[HttpGet("get-list")]
+		public IActionResult GetProjectList(int userId)
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost("create")]
+		public IActionResult CreateProject()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost("update")]
+		public IActionResult UpdateProject()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpDelete("delete")]
+		public IActionResult DeleteProject()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ react/strive-server/Strive/Strive.Tests/API/Projects/ProjectsControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Strive.API.Controllers;
+
+namespace Strive.Tests.API.Projects
+{
+	public class ProjectsControllerTests
+	{
+		public ProjectsControllerTests()
+		{
+		}
+
+		public ProjectsController ProjectsControllerInstance
+		{
+			get
+			{
+				return new ProjectsController();
+			}
+		}
+	}
+}
