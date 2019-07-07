c307dfc2caaec408fea71087ae0e7bac34244872
Pavel Kirilin <kirilin.pav@gmail.com>
2019-07-07 21:21:56 +0300

    [server] Added unit tests for custom exception handler middleware

---
+++ strive-server/src/Strive/Strive.Tests/Middlewares/StriveExceptionHandlerMiddlewareTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Strive.API.Middlewares;
+using Strive.Data.Dtos;
+using Strive.Exceptions;
+using Xunit;
+
+namespace Strive.Tests.Middlewares
+{
+    public class StriveExceptionHandlerMiddlewareTests
+    {
+
+        [Fact]
+        public async Task HandlesStriveExceptionCorrectly()
+        {
+            //----------------------------------------------
+            // Arrange
+            //----------------------------------------------
+
+            var expectedException = new StriveException("test message", "test description");
+
+            var middleware = new StriveExceptionHandlerMiddleware(innerContext =>
+            {
+                throw expectedException;
+            });
+
+            var context = new DefaultHttpContext();
+            context.Response.Body = new MemoryStream();
+
+            //----------------------------------------------
+            // Act
+            //----------------------------------------------
+
+            await middleware.Invoke(context);
+
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+            var reader = new StreamReader(context.Response.Body);
+            var result = JsonConvert.DeserializeObject(
+                reader.ReadToEnd(), typeof(ErrorResponseDto))
+                as ErrorResponseDto;
+
+            //----------------------------------------------
+            // Assert
+            //----------------------------------------------
+
+            Assert.NotNull(result);
+
+            Assert.Equal(StatusCodes.Status500InternalServerError, context.Response.StatusCode);
+            Assert.Equal("application/json", context.Response.ContentType);
+
+            Assert.Equal(expectedException.Message, result.Message);
+            Assert.Equal(expectedException.Description, result.Description);
+        }
+
+        [Fact]
+        public async Task HandlesCommonExceptionCorrectly()
+        {
+            //----------------------------------------------
+            // Arrange
+            //----------------------------------------------
+
+            string exceptionMessage = "Internal server error";
+            string exceptionDescription = "Unexpected error occured while processing the request";
+
+            var expectedException = new Exception("test message");
+
+            var middleware = new StriveExceptionHandlerMiddleware(innerContext =>
+            {
+                throw expectedException;
+            });
+
+            var context = new DefaultHttpContext();
+            context.Response.Body = new MemoryStream();
+
+            //----------------------------------------------
+            // Act
+            //----------------------------------------------
+
+            await middleware.Invoke(context);
+
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+            var reader = new StreamReader(context.Response.Body);
+            var result = JsonConvert.DeserializeObject(
+                    reader.ReadToEnd(), typeof(ErrorResponseDto))
+                as ErrorResponseDto;
+
+            //----------------------------------------------
+            // Assert
+            //----------------------------------------------
+
+            Assert.NotNull(result);
+
+            Assert.Equal(StatusCodes.Status500InternalServerError, context.Response.StatusCode);
+            Assert.Equal("application/json", context.Response.ContentType);
+
+            Assert.Equal(exceptionMessage, result.Message);
+            Assert.Equal(exceptionDescription, result.Description);
+        }
+    }
+}
