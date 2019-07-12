7ca7d21215b756d57dc225762a2eb19a9c0267d4
Pavel Kirilin <kirilin.pav@gmail.com>
2019-07-12 12:13:58 +0300

    [server] Added StriveSecurityException handling to return unauthorized

--- strive-server/src/Strive/Strive.API/Middlewares/StriveExceptionHandlerMiddleware.cs
+++ strive-server/src/Strive/Strive.API/Middlewares/StriveExceptionHandlerMiddleware.cs
@@ -23,13 +23,20 @@ namespace Strive.API.Middlewares
             {
                 await _nextRequest.Invoke(context);
             }
+            catch (StriveSecurityException se)
+            {
+                await HandleExceptionAsync(context, se, StatusCodes.Status401Unauthorized);
+            }
             catch (Exception e)
             {
                 await HandleExceptionAsync(context, e);
             }
         }
 
-        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        private async Task HandleExceptionAsync(
+            HttpContext context, 
+            Exception exception, 
+            int statusCode = StatusCodes.Status500InternalServerError)
         {
             string exceptionMessage = "Internal server error";
             string exceptionDescription = "Unexpected error occured while processing the request";
@@ -41,10 +48,12 @@ namespace Strive.API.Middlewares
             }
 
             context.Response.ContentType = "application/json";
-            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.StatusCode = statusCode;
 
-            var settings = new JsonSerializerSettings();
-            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
 
             await context.Response.WriteAsync(JsonConvert.SerializeObject(
                 new ErrorResponseDto()
--- strive-server/src/Strive/Strive.Tests/Middlewares/StriveExceptionHandlerMiddlewareTests.cs
+++ strive-server/src/Strive/Strive.Tests/Middlewares/StriveExceptionHandlerMiddlewareTests.cs
@@ -12,7 +12,6 @@ namespace Strive.Tests.Middlewares
 {
     public class StriveExceptionHandlerMiddlewareTests
     {
-
         [Fact]
         public async Task HandlesStriveExceptionCorrectly()
         {
@@ -50,6 +49,49 @@ namespace Strive.Tests.Middlewares
             Assert.NotNull(result);
 
             Assert.Equal(StatusCodes.Status500InternalServerError, context.Response.StatusCode);
+            Assert.Equal("application/json", context.Response.ContentType);
+
+            Assert.Equal(expectedException.Message, result.Message);
+            Assert.Equal(expectedException.Description, result.Description);
+        }
+
+        [Fact]
+        public async Task HandlesStriveSecurityExceptionCorrectly()
+        {
+            //----------------------------------------------
+            // Arrange
+            //----------------------------------------------
+
+            var expectedException = new StriveSecurityException("test message", "test description");
+
+            var middleware = new StriveExceptionHandlerMiddleware(innerContext =>
+            {
+                throw expectedException;
+            });
+
+            var context = new DefaultHttpContext();
+            context.Response.Body = new MemoryStream();
+
+            //----------------------------------------------
+            // Act
+            //----------------------------------------------
+
+            await middleware.Invoke(context);
+
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+            var reader = new StreamReader(context.Response.Body);
+            var result = JsonConvert.DeserializeObject(
+                    reader.ReadToEnd(), typeof(ErrorResponseDto))
+                as ErrorResponseDto;
+
+            //----------------------------------------------
+            // Assert
+            //----------------------------------------------
+
+            Assert.NotNull(result);
+
+            Assert.Equal(StatusCodes.Status401Unauthorized, context.Response.StatusCode);
             Assert.Equal("application/json", context.Response.ContentType);
 
             Assert.Equal(expectedException.Message, result.Message);
